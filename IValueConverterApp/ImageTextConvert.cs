9ef35d3bad9e32ccbbfc3df1585fe8c18fad646b
Jay <zuojianye@live.com>
2015-11-06 14:52:58 +0800

    init

---
+++ IValueConverterApp/ImageTextConvert.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace IValueConverterApp
+{
+    public class ImageTextConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string light = value as string;
+            switch (light)
+            {
+                case "Gray":
+                    return @"ms-appx:/Assets/Icon/GrayLightTransBack.png";
+
+                case "Green":
+                    return @"ms-appx:/Assets/Icon/GreenLightTransBack.png";
+
+                case "Red":
+                    return @"ms-appx:/Assets/Icon/RedLightTransBack.png";
+
+                case "Yellow":
+                    return @"ms-appx:/Assets/Icon/YellowLightTransBack.png";
+
+                default:
+                    return @"ms-appx:/Assets/Icon/GrayLightTransBack.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            //BitmapImage sentImage = ((ImageBrush)value).ImageSource as BitmapImage;
+            BitmapImage sentImage = (BitmapImage)value;
+            System.Diagnostics.Debug.WriteLine("Convert  " + sentImage.UriSource);
+            if (sentImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/GrayLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return "Gray";
+            }
+            else if (sentImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/GreenLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return "Green";
+            }
+            else if (sentImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/RedLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return "Red";
+            }
+            else if (sentImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/YellowLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return "Yellow";
+            }
+
+            return null;
+        }
+    }
+}
---
+++ IValueConverterApp/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IValueConverterApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void StoplightButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button stopLightButton = (Button)sender;
+            ImageBrush sentImage = (ImageBrush)stopLightButton.Background;
+            BitmapImage buttonImage = (BitmapImage)sentImage.ImageSource;
+
+            if (buttonImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/GrayLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                ((ImageBrush)stopLightButton.Background).ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/RedLightTransBack.png"));
+            }
+            else if (buttonImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/RedLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                ((ImageBrush)stopLightButton.Background).ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/YellowLightTransBack.png"));
+            }
+            else if (buttonImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/YellowLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                ((ImageBrush)stopLightButton.Background).ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/GreenLightTransBack.png"));
+            }
+            else if (buttonImage.UriSource.ToString()
+                .Equals(@"ms-appx:/Assets/Icon/GreenLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            {
+                ((ImageBrush)stopLightButton.Background).ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/GrayLightTransBack.png"));
+            }
+
+            //Button stopLightButton = (Button)sender;
+            //ImageBrush sentImage = (ImageBrush)stopLightButton.Background;
+            //BitmapImage buttonImage = (BitmapImage)sentImage.ImageSource;
+
+            //if (buttonImage.UriSource.ToString()
+            //    .Equals(@"ms-appx:/Assets/Icon/GrayLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            //{
+            //    var brush = new ImageBrush();
+            //    brush.ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/RedLightTransBack.png"));
+            //    stopLightButton.Background = brush;
+            //}
+            //else if (buttonImage.UriSource.ToString()
+            //    .Equals(@"ms-appx:/Assets/Icon/RedLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            //{
+            //    var brush = new ImageBrush();
+            //    brush.ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/YellowLightTransBack.png"));
+            //    stopLightButton.Background = brush;
+            //}
+            //else if (buttonImage.UriSource.ToString()
+            //    .Equals(@"ms-appx:/Assets/Icon/YellowLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            //{
+            //    var brush = new ImageBrush();
+            //    brush.ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/GreenLightTransBack.png"));
+            //    stopLightButton.Background = brush;
+            //}
+            //else if (buttonImage.UriSource.ToString()
+            //    .Equals(@"ms-appx:/Assets/Icon/GreenLightTransBack.png", StringComparison.CurrentCultureIgnoreCase))
+            //{
+            //    var brush = new ImageBrush();
+            //    brush.ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/GrayLightTransBack.png"));
+            //    stopLightButton.Background = brush;
+            //}
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine(IB.ImageSource);
+            //IB.ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/RedLightTransBack.png"));
+            //System.Diagnostics.Debug.WriteLine(((BitmapImage)IB.ImageSource).UriSource);
+
+            //var brush = new ImageBrush();
+            //brush.ImageSource = new BitmapImage(new Uri(@"ms-appx:/Assets/Icon/GrayLightTransBack.png"));
+            //StoplightButton.Background = brush;
+        }
+    }
+}
